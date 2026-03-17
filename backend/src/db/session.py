1304be61cf2b2a854037d7f12794f9ec96e18577
Olayiwola Theophilus <Olayiwolatheophilusayomide@gmail.com>
2026-03-17 12:31:55 +0100

    db session functions created

---
+++ backend/src/db/session.py
@@ -0,0 +1,16 @@
+import sqlmodel
+from sqlmodel import Session, SQLModel
+
+from src.core.config import DATABASE_URL
+
+engine = sqlmodel.create_engine(DATABASE_URL)
+
+#init db
+def init_db():
+    print("creating database tables")
+    SQLModel.metadata.create_all(engine)
+
+#get db sessions
+def get_session():
+    with Session(engine) as session:
+        yield session
