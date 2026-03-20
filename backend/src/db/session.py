7616da7f10d5109c5500ae2f04bb716002a2d2cf
Olayiwola Theophilus <Olayiwolatheophilusayomide@gmail.com>
2026-03-20 21:52:49 +0100

    db async

--- backend/src/db/session.py
+++ backend/src/db/session.py
@@ -1,16 +1,29 @@
 import sqlmodel
+import sqlalchemy
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlmodel import Session, SQLModel
 
 from src.core.config import settings
 
-engine = sqlmodel.create_engine(settings.DATABASE_URL)
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    echo=True,
+    future=True
+)
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
 
 #init db
-def init_db():
-    print("creating database tables")
-    SQLModel.metadata.create_all(engine)
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
 
 #get db sessions
-def get_session():
-    with Session(engine) as session:
+async def get_session():
+    async with AsyncSessionLocal() as session:
         yield session
